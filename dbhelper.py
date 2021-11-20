fe84948d09d56a9bd4ba4fbf2bafbeacd47d1190
rds8896 <ramansingh274@gmail.com>
2021-11-21 00:16:29 +0530

    Telegram bot files added

---
+++ dbhelper.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+
+class DBHelper:
+    def __init__(self, dbname="https://github.com/rds8896/my_channels/movie.sqlite"):
+        self.dbname = dbname
+        self.conn = sqlite3.connect(dbname)
+
+    def setup(self):
+        tblstmt = "CREATE TABLE IF NOT EXISTS items (movie_name text,url text, owner text)"
+        itemidx = "CREATE INDEX IF NOT EXISTS itemIndex ON items (movie_name ASC)"
+        ownidx = "CREATE INDEX IF NOT EXISTS ownIndex ON items (owner ASC)"
+        self.conn.execute(tblstmt)
+        self.conn.execute(itemidx)
+        self.conn.execute(ownidx)
+        self.conn.commit()
+
+    def add_item(self, movie, url, owner):
+        stmt = "INSERT INTO items (movie_name, url, owner) VALUES (?, ?, ?)"
+        args = (movie, url, owner)
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+
+    def delete_item(self, item_text, owner):
+        stmt = "DELETE FROM items WHERE description = (?) AND owner = (?)"
+        args = (item_text, owner)
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+
+    def get_items(self, owner):
+        stmt = "SELECT * FROM items WHERE owner = (?)"
+        args = (owner,)
+        return [x[0] for x in self.conn.execute(stmt, args)]
+
+
+
+
+
+
